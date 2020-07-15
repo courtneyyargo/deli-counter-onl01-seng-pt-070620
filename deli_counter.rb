cdb10d40f96e9d43f16d2a896687f0ace0a86d26
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-15 17:51:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+end 
